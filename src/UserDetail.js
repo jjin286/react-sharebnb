45169d31fe97de3274b766a38c616a97676f73ed
Silu Hu <erinh816@gmail.com>
2023-11-17 13:09:02 -0500

    figruing out spacing

--- src/UserDetail.js
+++ src/UserDetail.js
@@ -1,18 +1,23 @@
 import BookingsList from "./BookingsList";
+import "./UserDetail.css";
 
-function UserDetail({user}){
+function UserDetail({ user }) {
 
-  console.log("user bookings", user.Bookings)
-  return(
+  console.log("user bookings", user.Bookings);
+  return (
     <div className="UserDetail">
-      <h1>{user.username}</h1>
-      <p>Email: {user.email}</p>
-      <p>First name: {user.first_name}</p>
-      <p>Last name: {user.last_name}</p>
-      <p>Host: {user.is_host ? "Yes" : "No"}</p>
-      <BookingsList bookings={user.Bookings} />
+      <div className="UserCard">
+        <h1>{user.username}</h1>
+        <p>Email: {user.email}</p>
+        <p>First name: {user.first_name}</p>
+        <p>Last name: {user.last_name}</p>
+        <p>Host: {user.is_host ? "Yes" : "No"}</p>
+      </div>
+      <div className="UserDetail-list">
+        <BookingsList bookings={user.Bookings} />
+      </div>
     </div>
-  )
+  );
 }
 
 export default UserDetail;
